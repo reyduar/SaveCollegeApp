b3ddd52a2a02efd7dd36191d087e91d8f0c1b1e4
ariel.duarte <arielpy@gmail.com>
2023-02-01 21:09:57 -0300

    Added initial project files

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const studens = [
+  {
+    name: "Cesar",
+    room: "Pantanal",
+    age: 20,
+    gender: "feminino",
+    height: 1.87,
+  },
+  {
+    name: "Cynthia",
+    room: "Coqueiros",
+    age: 45,
+    gender: "masculino",
+    height: 1.5,
+  },
+  {
+    name: "Daniel",
+    room: " Itaguaçu",
+    age: 62,
+    gender: "masculino",
+    height: 1.7,
+  },
+  {
+    name: "Andressa",
+    room: "Palhoça",
+    age: 80,
+    gender: "masculino",
+    height: 1.45,
+  },
+  {
+    name: "Camila",
+    room: "Trindade",
+    age: 5,
+    gender: "masculino",
+    height: 1.55,
+  },
+];
+studens.forEach((student) => {
+  console.log("====================================");
+  console.log(`Nome: ${student.name}`);
+  console.log(`Sala: ${student.room}`);
+  console.log(`Idade: ${student.age}`);
+  console.log(`Sexo: ${student.gender}`);
+  console.log(`Altura: ${student.height}`);
+  console.log("====================================");
+});
