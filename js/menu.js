b35aee41ad1b12dbecd21fed146dd0660eb86ffe
ariel.duarte <arielpy@gmail.com>
2023-02-09 01:11:56 -0300

    M1S02-Ex2 - feat: Added login page

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
+function loginOnclick() {
+  window.location.replace(
+    "https://reyduar.github.io/SaveCollegeApp/login.html"
+  );
+}
+
 function menuOnclick(value) {
   document.querySelector("#semana1").style.display = "none";
   document.querySelector("#semana2").style.display = "none";
@@ -8,6 +14,7 @@ const menu = () => {
   document.querySelector("#semana2").style.display = "none";
   const menu = document.querySelector("#menu");
   menu.innerHTML = `
+  <button type="button" onclick="loginOnclick()">Login Page</button> 
   <button type="button" onclick="menuOnclick('semana1')">Semana 1</button> 
   <button type="button" onclick="menuOnclick('semana2')">Semana 2</button>`;
 };
